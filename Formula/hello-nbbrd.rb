94757be889c673010ae6e856560ab395f8a11704
jreleaserbot <jreleaser@kordamp.org>
2021-07-16 09:54:31 +0000

    hello-nbbrd v1.17.4

--- Formula/hello-nbbrd.rb
+++ Formula/hello-nbbrd.rb
@@ -1,9 +1,9 @@
 class HelloNbbrd < Formula
   desc "Sandbox - CLI"
   homepage "https://github.com/nbbrd/sandbox"
-  version "1.17.3"
-  url "https://github.com/nbbrd/sandbox/releases/download/v1.17.3/hello-nbbrd-1.17.3-bin.jar", :using => :nounzip
-  sha256 "25d9b3bb87df89e658bd95bebbf8dcc6302246982e32446d4092938dd75f820e"
+  version "1.17.4"
+  url "https://github.com/nbbrd/sandbox/releases/download/v1.17.4/hello-nbbrd-1.17.4-bin.jar", :using => :nounzip
+  sha256 "a3b0567593328701a131801d470930a009a8e643beff94a584a0ec6abfa5ae48"
   license "European Union Public Licence (EUPL)"
 
   bottle :unneeded
@@ -13,14 +13,14 @@ class HelloNbbrd < Formula
   def install
     File.open("hello-nbbrd", "w") do |f|
       f << "#!/bin/bash\n"
-      f << "java -jar $JAVA_OPTS #{prefix}/hello-nbbrd-1.17.3-bin.jar \"$@\"\n"
+      f << "java -jar $JAVA_OPTS #{prefix}/hello-nbbrd-1.17.4-bin.jar \"$@\"\n"
     end
-    prefix.install "hello-nbbrd-1.17.3-bin.jar"
+    prefix.install "hello-nbbrd-1.17.4-bin.jar"
     bin.install "hello-nbbrd"
   end
 
   test do
     output = shell_output("#{bin}/hello-nbbrd --version")
-    assert_match "1.17.3", output
+    assert_match "1.17.4", output
   end
 end
