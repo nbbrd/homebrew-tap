89d20ed442eb655e67df1246d89a1e1ff6c60a0a
jreleaserbot <jreleaser@kordamp.org>
2022-04-21 08:36:10 +0000

    sdmx-dl v3.0.0-beta.6

--- Formula/sdmx-dl.rb
+++ Formula/sdmx-dl.rb
@@ -1,20 +1,20 @@
+# Generated with JReleaser 1.0.0 at 2022-04-21T08:36:01.757975633Z
 class SdmxDl < Formula
   desc "Download SDMX data - CLI"
   homepage "https://github.com/nbbrd/sdmx-dl"
-  version "3.0.0-beta.5"
-  url "https://github.com/nbbrd/sdmx-dl/releases/download/v3.0.0-beta.5/sdmx-dl-3.0.0-beta.5-bin.jar", :using => :nounzip
-  sha256 "5f2cb61480b557711845a9f1e7625c19108e48d6eeab6f36d5251291194db49b"
+  url "https://github.com/nbbrd/sdmx-dl/releases/download/v3.0.0-beta.6/sdmx-dl-3.0.0-beta.6-bin.jar", :using => :nounzip
+  version "3.0.0-beta.6"
+  sha256 "bf5379aa4f429bb1524b160aa370079a9075bee7f9402e92bfe0a16a5929f41a"
   license "European Union Public Licence (EUPL)"
 
-  depends_on "openjdk@9"
 
   def install
-    libexec.install "sdmx-dl-3.0.0-beta.5-bin.jar"
-    bin.write_jar_script libexec/"sdmx-dl-3.0.0-beta.5-bin.jar", "sdmx-dl"
+    libexec.install "sdmx-dl-3.0.0-beta.6-bin.jar"
+    bin.write_jar_script libexec/"sdmx-dl-3.0.0-beta.6-bin.jar", "sdmx-dl"
   end
 
   test do
     output = shell_output("#{bin}/sdmx-dl --version")
-    assert_match "3.0.0-beta.5", output
+    assert_match "3.0.0-beta.6", output
   end
 end
