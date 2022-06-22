a8450ce08a50d2d74b9514e37135f71ec5f7c9d8
jreleaserbot <jreleaser@kordamp.org>
2022-06-22 12:39:45 +0000

    hello-nbbrd v2.2.2

--- Formula/hello-nbbrd.rb
+++ Formula/hello-nbbrd.rb
@@ -1,21 +1,21 @@
-# Generated with JReleaser 1.0.0 at 2022-04-19T14:29:51.069342546Z
+# Generated with JReleaser 1.0.0 at 2022-06-22T12:39:34.397919798Z
 class HelloNbbrd < Formula
   desc "Sandbox - Binaries"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.1.0/hello-nbbrd-2.1.0-bin.jar", :using => :nounzip
-  version "2.1.0"
-  sha256 "9614e437bca26769c7bf7930c900ffae9e1169c8dead4e6a4d212d1b93512866"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.2.2/hello-nbbrd-2.2.2-bin.jar", :using => :nounzip
+  version "2.2.2"
+  sha256 "f88cdc57d3b7dda8f8804abf915487d28bbef9e0dd39a1dd1a9d24f75198d896"
   license "European Union Public Licence (EUPL)"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "hello-nbbrd-2.1.0-bin.jar"
-    bin.write_jar_script libexec/"hello-nbbrd-2.1.0-bin.jar", "hello-nbbrd"
+    libexec.install "hello-nbbrd-2.2.2-bin.jar"
+    bin.write_jar_script libexec/"hello-nbbrd-2.2.2-bin.jar", "hello-nbbrd"
   end
 
   test do
     output = shell_output("#{bin}/hello-nbbrd --version")
-    assert_match "2.1.0", output
+    assert_match "2.2.2", output
   end
 end
