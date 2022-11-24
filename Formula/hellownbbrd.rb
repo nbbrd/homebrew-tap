dd019ad6edb541db5c8bb8c13644b46968a30573
jreleaserbot <jreleaser@kordamp.org>
2022-11-24 12:36:30 +0000

    hellow-nbbrd v2.2.7

--- Formula/hellownbbrd.rb
+++ Formula/hellownbbrd.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.3.1 at 2022-11-24T11:23:03.280084333Z
+# Generated with JReleaser 1.3.1 at 2022-11-24T12:36:15.51661816Z
 class HellowNbbrd < Formula
   desc "Sandbox - Bill of Materials"
   homepage "https://github.com/nbbrd/sandbox"
   url "https://github.com/nbbrd/sandbox/releases/download/v2.2.7/sandbox-desktop-2.2.7-bin.jar", :using => :nounzip
   version "2.2.7"
-  sha256 "1a36a8a877418d20ae834278f4a8b394f19c88a796c6d80d0e909c00c12831f2"
+  sha256 "fc783d3f52364681c6be8ed23bc8188d79faa72941418a4c861dc6411fc0d5ef"
   license "European Union Public Licence (EUPL)"
 
   depends_on "openjdk@8"
