6c0e98aa64fe888d33cd4c9b9735ec46c4e3ade2
jreleaserbot <jreleaser@kordamp.org>
2022-11-29 08:49:56 +0000

    heylogs v0.5.0

--- Formula/heylogs.rb
+++ Formula/heylogs.rb
@@ -1,20 +1,20 @@
-# Generated with JReleaser 1.2.0 at 2022-10-20T16:13:43.285534679Z
+# Generated with JReleaser 1.3.1 at 2022-11-29T08:49:46.230870767Z
 class Heylogs < Formula
   desc "Keep-a-changelog tool - CLI"
   homepage "https://github.com/nbbrd/heylogs"
-  url "https://github.com/nbbrd/heylogs/releases/download/v0.4.0/heylogs-cli-0.4.0-bin.jar", :using => :nounzip
-  version "0.4.0"
-  sha256 "4c470284398549f8f65ab9000ee52bf1971ad884b5a4de4754079b113878981c"
+  url "https://github.com/nbbrd/heylogs/releases/download/v0.5.0/heylogs-cli-0.5.0-bin.jar", :using => :nounzip
+  version "0.5.0"
+  sha256 "d3bebd2f32859418ecf0351534c58255634d9a2745d13beec76417c8fae0f3f2"
   license "European Union Public Licence (EUPL)"
 
 
   def install
-    libexec.install "heylogs-cli-0.4.0-bin.jar"
-    bin.write_jar_script libexec/"heylogs-cli-0.4.0-bin.jar", "heylogs"
+    libexec.install "heylogs-cli-0.5.0-bin.jar"
+    bin.write_jar_script libexec/"heylogs-cli-0.5.0-bin.jar", "heylogs"
   end
 
   test do
     output = shell_output("#{bin}/heylogs --version")
-    assert_match "0.4.0", output
+    assert_match "0.5.0", output
   end
 end
