1db391512bf64943274951d1dd88de72fd5ee307
jreleaserbot <jreleaser@kordamp.org>
2023-06-28 07:31:01 +0000

    hello-nbbrd v2.3.1

--- Formula/hello-nbbrd.rb
+++ Formula/hello-nbbrd.rb
@@ -1,29 +1,29 @@
-# Generated with JReleaser 1.7.0 at 2023-06-28T07:19:05.336009237Z
+# Generated with JReleaser 1.7.0 at 2023-06-28T07:30:49.965098004Z
 class HelloNbbrd < Formula
   desc "Sandbox - Bill of Materials"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.3.0/sandbox-cli-2.3.0-bin.jar", :using => :nounzip
-  version "2.3.0"
-  sha256 "0cf873b97ad81813bc6b40cd5ad10ed3bfa73b9414d9bff490bc4aad2b8adb19"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.3.1/sandbox-cli-2.3.1-bin.jar", :using => :nounzip
+  version "2.3.1"
+  sha256 "f734e1e43de21893b297a32f0b96ee5105588fd183bfe6b22311c42b0e0e082f"
   license "European Union Public Licence (EUPL)"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "sandbox-cli-2.3.0-bin.jar"
+    libexec.install "sandbox-cli-2.3.1-bin.jar"
 
     bin.mkpath
     File.open("#{bin}/hello-nbbrd", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-cli-2.3.0-bin.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-cli-2.3.1-bin.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/hello-nbbrd --version")
-    assert_match "2.3.0", output
+    assert_match "2.3.1", output
   end
 end
