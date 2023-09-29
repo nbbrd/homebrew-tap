00c413654ddc049a6aaeafd88397729eafb65b4d
jreleaserbot <jreleaser@kordamp.org>
2023-09-29 11:18:23 +0000

    hellow-nbbrd v2.3.3

--- Formula/hellow-nbbrd.rb
+++ Formula/hellow-nbbrd.rb
@@ -1,29 +1,29 @@
-# Generated with JReleaser 1.8.0 at 2023-09-19T12:11:07.015679434Z
+# Generated with JReleaser 1.8.0 at 2023-09-29T11:18:10.022574312Z
 class HellowNbbrd < Formula
-  desc "Sandbox - Bill of Materials"
+  desc "Hello NBBRD"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.3.2/sandbox-desktop-2.3.2-bin.jar", :using => :nounzip
-  version "2.3.2"
-  sha256 "852a124df8b42587ba08e115c756fef95b97135b6abdc211f1cc27267dc9010a"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.3.3/sandbox-desktop-2.3.3-bin.jar", :using => :nounzip
+  version "2.3.3"
+  sha256 "7bcec8f337235a4542df283212c712965a060698544e8d75c952f4167b25c442"
   license "European Union Public Licence (EUPL)"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "sandbox-desktop-2.3.2-bin.jar"
+    libexec.install "sandbox-desktop-2.3.3-bin.jar"
 
     bin.mkpath
     File.open("#{bin}/hellow-nbbrd", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-desktop-2.3.2-bin.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-desktop-2.3.3-bin.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/hellow-nbbrd --version")
-    assert_match "2.3.2", output
+    assert_match "2.3.3", output
   end
 end
