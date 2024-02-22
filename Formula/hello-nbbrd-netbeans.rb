15b4d73c952e219f7e44074f2f2959e1bd4d5885
jreleaserbot <jreleaser@kordamp.org>
2024-02-22 13:51:41 +0000

    hello-nbbrd-netbeans v2.4.2

--- Formula/hello-nbbrd-netbeans.rb
+++ Formula/hello-nbbrd-netbeans.rb
@@ -1,21 +1,21 @@
-# Generated with JReleaser 1.10.0 at 2024-02-20T11:04:05.075653481Z
+# Generated with JReleaser 1.10.0 at 2024-02-22T13:51:08.797569343Z
 
 class HelloNbbrdNetbeans < Formula
   desc "Hello NBBRD"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.4.1/sandbox-netbeans-app-2.4.1-mac-x86_64.zip"
-  version "2.4.1"
-  sha256 "8ea65db8b0862e573ebdbb04cd36c2b17ea7f6b6d2723e8362786c3415f65f36"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.4.2/sandbox-netbeans-app-2.4.2-osx-x86_64.zip"
+  version "2.4.2"
+  sha256 "5964740a3a43b1d972edd24872a0c9ca13e46bd2f931fd487cb58c7de662c5e3"
   license "European Union Public Licence (EUPL)"
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/sandboxnetbeans" => "sandboxnetbeans"
+    bin.install_symlink "#{libexec}/bin/hello-nbbrd-netbeans" => "hello-nbbrd-netbeans"
   end
 
   test do
-    output = shell_output("#{bin}/sandboxnetbeans --version")
-    assert_match "2.4.1", output
+    output = shell_output("#{bin}/hello-nbbrd-netbeans --version")
+    assert_match "2.4.2", output
   end
 end
