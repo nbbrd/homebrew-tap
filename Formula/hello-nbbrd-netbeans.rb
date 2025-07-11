3bb009066b1d3baaf2744fe03cb7621fd4362f80
jreleaserbot <jreleaser@kordamp.org>
2025-07-11 10:18:35 +0000

    hello-nbbrd-netbeans v2.6.0

--- Formula/hello-nbbrd-netbeans.rb
+++ Formula/hello-nbbrd-netbeans.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.18.0 at 2025-05-19T13:39:16.869831384Z
+# Generated with JReleaser 1.19.0 at 2025-07-11T10:17:50.157033096Z
 
 class HelloNbbrdNetbeans < Formula
   desc "Hello NBBRD"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.5.2/sandbox-netbeans-standalone-2.5.2-osx-x86_64.zip"
-  version "2.5.2"
-  sha256 "b74c79e17e45d0ae6e99817dc7ab5bdf0580322d09ede42dff16b579da3894b9"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.6.0/sandbox-netbeans-standalone-2.6.0-osx-x86_64.zip"
+  version "2.6.0"
+  sha256 "d2f8a26445db3fe11bb477e1a9b1b467e272bcd8493eac3f802dc0192fbeb94a"
   license "European Union Public Licence (EUPL)"
 
 
@@ -16,6 +16,6 @@ class HelloNbbrdNetbeans < Formula
 
   test do
     output = shell_output("#{bin}/hello-nbbrd-netbeans --version")
-    assert_match "2.5.2", output
+    assert_match "2.6.0", output
   end
 end
