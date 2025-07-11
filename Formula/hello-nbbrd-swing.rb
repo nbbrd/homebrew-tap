394d9ef0656fb2ebd185ff4da06abeaacc427228
jreleaserbot <jreleaser@kordamp.org>
2025-07-11 10:41:34 +0000

    hello-nbbrd-swing v2.6.1

--- Formula/hello-nbbrd-swing.rb
+++ Formula/hello-nbbrd-swing.rb
@@ -1,30 +1,30 @@
-# Generated with JReleaser 1.19.0 at 2025-07-11T10:17:50.157033096Z
+# Generated with JReleaser 1.19.0 at 2025-07-11T10:40:46.892313101Z
 
 class HelloNbbrdSwing < Formula
   desc "Hello NBBRD"
   homepage "https://github.com/nbbrd/sandbox"
-  url "https://github.com/nbbrd/sandbox/releases/download/v2.6.0/sandbox-swing-2.6.0-bin.jar", :using => :nounzip
-  version "2.6.0"
-  sha256 "7d47ccc40b73352c3056f5a65d8d43a1d6672cbbf5789d8de6895bc11e89a63a"
+  url "https://github.com/nbbrd/sandbox/releases/download/v2.6.1/sandbox-swing-2.6.1-bin.jar", :using => :nounzip
+  version "2.6.1"
+  sha256 "dd1d84c541736e798ea31d9afd396167c97f37c391527c35ea69abdfc8729801"
   license "European Union Public Licence (EUPL)"
 
   depends_on "openjdk@8"
 
   def install
-    libexec.install "sandbox-swing-2.6.0-bin.jar"
+    libexec.install "sandbox-swing-2.6.1-bin.jar"
 
     bin.mkpath
     File.open("#{bin}/hello-nbbrd-swing", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-swing-2.6.0-bin.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sandbox-swing-2.6.1-bin.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/hello-nbbrd-swing --version")
-    assert_match "2.6.0", output
+    assert_match "2.6.1", output
   end
 end
