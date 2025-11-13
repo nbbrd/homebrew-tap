cab4674afcea4045f0971fd3243941ab5beb7a68
jreleaserbot <jreleaser@kordamp.org>
2025-11-13 13:49:35 +0000

    sdmx-dl v3.0.0-beta.16

--- Formula/sdmx-dl.rb
+++ Formula/sdmx-dl.rb
@@ -1,29 +1,29 @@
-# Generated with JReleaser 1.20.0 at 2025-10-24T12:31:31.289783974Z
+# Generated with JReleaser 1.21.0 at 2025-11-13T13:49:23.452520757Z
 
 class SdmxDl < Formula
   desc "Easily download official statistics"
   homepage "https://github.com/nbbrd/sdmx-dl"
-  url "https://github.com/nbbrd/sdmx-dl/releases/download/v3.0.0-beta.15/sdmx-dl-cli-3.0.0-beta.15-bin.jar", :using => :nounzip
-  version "3.0.0-beta.15"
-  sha256 "d656febd655ddf6abb247c9ce5bf517d938181dc7eff2b272d748b8b2b548efa"
+  url "https://github.com/nbbrd/sdmx-dl/releases/download/v3.0.0-beta.16/sdmx-dl-cli-3.0.0-beta.16-bin.jar", :using => :nounzip
+  version "3.0.0-beta.16"
+  sha256 "8aa120052760559a53218952da1f4475c2c670ffef5888422b97b2567d19f040"
   license "European Union Public Licence (EUPL)"
 
 
   def install
-    libexec.install "sdmx-dl-cli-3.0.0-beta.15-bin.jar"
+    libexec.install "sdmx-dl-cli-3.0.0-beta.16-bin.jar"
 
     bin.mkpath
     File.open("#{bin}/sdmx-dl", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sdmx-dl-cli-3.0.0-beta.15-bin.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/sdmx-dl-cli-3.0.0-beta.16-bin.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/sdmx-dl --version")
-    assert_match "3.0.0-beta.15", output
+    assert_match "3.0.0-beta.16", output
   end
 end
